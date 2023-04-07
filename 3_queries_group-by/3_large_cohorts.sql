acf1efbcbc052893cfb3f39f88bc2a2238a5195e
eliachow <eliacchow@gmail.com>
2023-04-07 10:40:48 -0700

    create group by queries

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,15 @@
+-- SELECT cohorts.name as cohort_name, 
+-- count(students.*) as student_count
+-- FROM cohorts
+-- JOIN students ON students.cohort_id = cohorts.id
+-- GROUP BY cohorts.name
+-- HAVING count(students.*) >= 18
+-- ORDER BY student_count;
+
+
+SELECT cohorts.name as cohort_name, count(students.*) AS student_count 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) >= 18
+ORDER BY student_count;
